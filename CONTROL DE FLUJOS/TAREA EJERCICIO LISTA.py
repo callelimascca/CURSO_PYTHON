4272aeb92648a0d7e34e0463c502e0fb668d2220
callelimascca <callelimasccamaria13@gmail.com>
2023-06-23 10:12:09 -0500

    Tarea realizada de lista 23/06/2023

---
+++ CONTROL DE FLUJOS/TAREA EJERCICIO LISTA.py
@@ -0,0 +1,11 @@
+###hacer un programa que pida datos y que cuando llegue a pedir el 5 dato se corte la ejecucion y muestre la lista completa
+lista = []
+for i in range(5):
+    datos = input("Ingrese los datos: ")
+    lista.append(datos)
+print(f"""
+      **********************
+      LA LISTA COMPLETA ES 
+      **********************
+      """)
+print(lista)
--- CONTROL DE FLUJOS/listas.py
+++ CONTROL DE FLUJOS/listas.py
@@ -6,3 +6,15 @@ while True:
             break
     lista.append(datos)
 print(lista)
+
+##### lista con condicion
+
+lista=[]
+condicion=1
+while condicion:
+    datos=input("Ingresa el primer dato: ")
+    if datos == "si":
+            condicion=0
+    lista.append(datos)
+print(lista)
+###Ingresar datos y que cuando llegue a 5 datos deje de pedir datos y corte la ejecucion muestre la lista completa
