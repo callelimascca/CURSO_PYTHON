2885cd3be0c46a2b7bc4f131c510801a775ba405
callelimascca <callelimasccamaria13@gmail.com>
2023-06-27 10:01:46 -0500

    27/06/2023

--- CONTROL DE FLUJOS/tabla de multiplicar.py
+++ CONTROL DE FLUJOS/tabla de multiplicar.py
@@ -3,3 +3,12 @@ tablaDe= int(input("Ingresa un numero: "))
 for numero in range(1, 13):
     resultado=numero * tablaDe
     print(f"{numero} * {tablaDe} = {resultado}")
+
+    ##HACER UN PROGRAMA QUE PIDA UN NUMERO Y CALCULE SU FACTORIAL
+    ##EJEMPLO SI INGRESO 5 
+    # DE SALIDA DE MOSTRAR O IMPRIMIR 120
+num = int(input("INGRESA UN NUMERO: "))
+factorial = 1
+for numero in range(1, num + 1):
+        factorial = factorial * numero
+print("EL FACTORIAL DE", num, "ES", factorial)
